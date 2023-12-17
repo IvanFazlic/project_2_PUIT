a917ce28f59b21d8398adb1cf03cece5d3f2e069
Ognjen Baltic RIN-3/22 <ognjenrin322@gs.viser.edu.rs>
2023-12-17 23:31:41 +0100

    Sklonjena labela sa tekstom originalni, ispisan tekst Original i Sharpened na samim slikama nakon obrade

--- SharpenModule/SharpenFrame.py
+++ SharpenModule/SharpenFrame.py
@@ -1,6 +1,7 @@
+import copy
 import tkinter as tk
 from tkinter import filedialog
-from PIL import Image, ImageTk, ImageFilter
+from PIL import Image, ImageTk, ImageFilter, ImageDraw, ImageFont
 # import numpy as np
 
 
@@ -28,12 +29,11 @@ class SharpenFrame(tk.Frame):
         self.spinbox = tk.Spinbox(self.control, command=self.spinbox_changed, from_=0, to=10, width=3,
                                   textvariable=self.sharpen_multiplier)
         self.spinbox.grid(row=0, column=1)
-        self.lblOrig = tk.Label(self.control, text="Originalna slika")
-        self.lblOrig.grid(row=1, column=0, sticky="w")
         self.btnBrowse = tk.Button(self.control, text="Otvori novu sliku", command=self.browse_clicked)
-        self.btnBrowse.grid(row=0, column=3, rowspan=2, sticky="NS")
+        self.btnBrowse.grid(row=0, column=3, sticky="NS")
         self.control.grid(row=0, column=0,sticky="new")
         # images setup
+        self.font = ImageFont.truetype("arial.ttf", size=20)
         self.width = 400
         self.height = 226
         self.images = tk.Frame(self)
@@ -82,7 +82,9 @@ class SharpenFrame(tk.Frame):
         else:
             self.imgObj = self.imgObjOrig.resize((self.width,
                                                   int(self.imgObjOrig.height/self.imgObjOrig.width*self.width)))
-        image = ImageTk.PhotoImage(self.imgObj)
+        img = copy.deepcopy(self.imgObj)
+        ImageDraw.Draw(img).text((10, 10), "Original", fill=(255, 0, 0), font=self.font)
+        image = ImageTk.PhotoImage(img)
         self.lblImage = tk.Label(self.images, image=image)
         self.lblImage.image = image
         #self.lblImage.pack()
@@ -95,9 +97,10 @@ class SharpenFrame(tk.Frame):
         for i in range(self.sharpen_multiplier.get()):
             # shrpn = shrpn.filter(ImageFilter.Kernel((3, 3), self.sharpen_kernel)) # custom kernel
             shrpn = shrpn.filter(ImageFilter.SHARPEN)
-        image_sharpened = ImageTk.PhotoImage(shrpn)
+        sharpened = copy.deepcopy(shrpn)
+        ImageDraw.Draw(sharpened).text((10, 10), "Sharpened", fill=(255, 0, 0), font=self.font)
+        image_sharpened = ImageTk.PhotoImage(sharpened)
         self.lblShrpn = tk.Label(self.images, image=image_sharpened)
         self.lblShrpn.image = image_sharpened
-        #self.lblShrpn.pack()
         self.lblShrpn.grid(row=2,column=0,sticky="nsew")
         self.pack()
