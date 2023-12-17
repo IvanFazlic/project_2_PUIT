ddca566259fbb4d1d0f959ad0f6b1d6d56790b00
Ivan <fazlic.ivan00@gmail.com>
2023-12-17 21:31:07 +0100

    Gotova prva verzija

--- resavanjeSistema.py
+++ resavanjeSistema.py
@@ -3,51 +3,70 @@ from tkinter import messagebox
 
 
 def ispisi(n, m, a):
+    s = ""
     for i in range(n):
         for j in range(n + m):
-            print('{:8.2f}'.format(a[i][j]), end='')
-        print("")
-    print("")
+            s += '{:8.2f} '.format(a[i][j])
+        s += '\n'
+    s += '\n'
+    return s
 
 
-def show_solution(window, a):
-    eps = 0.00001
-    n = 3
+def show_solution(r, a):
+    v = tk.Scrollbar(r)
+    v.pack(side='right', fill='y')
+
+    t = tk.Text(r, width=15, height=20, wrap=tk.NONE, yscrollcommand=v.set)
+
     m = 1 + 3
+    n = 3
+    t.insert(tk.END, f"1. Pocetna matrica je: \n{ispisi(n, m, a)}")
+    t.pack(side=tk.TOP, fill=tk.X)
+    eps = 0.00001
 
-    ispisi(n, m, a)
-
-    # Gaus-Jordan eliminacija
+    # Gaus-Jordan
     for k in range(n):
         # Da li je pivot element premali
+        t.insert(tk.END, f"2. Da li je pivot element premali?\n")
+        t.pack(side=tk.TOP, fill=tk.X)
         if abs(a[k][k]) <= eps:
-            print("Mala vrednost pivot-a!")
+            messagebox.showerror("Error", "Mala vrednost pivot-a!")
             exit(0)
-
+        t.insert(tk.END, f" Nije.\n")
+        t.pack(side=tk.TOP, fill=tk.X)
         # Normalizacija pivot reda
+        t.insert(tk.END, f"3. Normalizacija pivot reda: \n")
+        t.pack(side=tk.TOP, fill=tk.X)
         for j in range(k + 1, n + m):
             a[k][j] = a[k][j] / a[k][k]
         a[k][k] = 1
-        print("a[k][k]=a[%d][%d]=1, normalizacija reda %d" % (k, k, k))
-        ispisi(n, m, a)
+        t.insert(tk.END, "a[k][k]=a[{}][{}]=1, normalizacija reda {}\n".format(k, k, k) + f"{ispisi(n, m, a)}")
+        t.pack(side=tk.TOP, fill=tk.X)
 
         # Eliminacija k-tog elementa osim pivota
+        t.insert(tk.END, f"4. Eliminacija k-tog elementa osim pivota: \n")
+        t.pack(side=tk.TOP, fill=tk.X)
         for i in range(n):
             if i == k or a[i][k] == 0:
-                print("(i==k || a[i][k]==0), a[i][k]=a[%d][%d]=%6.2lf, continue" % (i, k, a[i][k]))
-                ispisi(n, m, a)
+                t.insert(tk.END, "(i==k || a[i][k]==0), a[i][k]=a[{}][{}]={:.2f}, nastavi..\n".format(i, k, a[i][
+                    k]) + f"{ispisi(n, m, a)}")
+                t.pack(side=tk.TOP, fill=tk.X)
                 continue
 
             for j in range(k + 1, n + m):
                 a[i][j] = a[i][j] - a[i][k] * a[k][j]
-            print("a[i][j]=a[%d][%d]=%6.2lf racunanje a[i][j] - a[i][k] * a[k][j], k=%d" % (i, j, a[i][j], k))
-            ispisi(n, m, a)
+                t.insert(tk.END, "a[i][j]=a[{}][{}]={:.2f} racunanje a[i][j] - a[i][k] * a[k][j],"
+                                 " k={}\n".format(i, j, a[i][j], k) + f"{ispisi(n, m, a)}")
+                t.pack(side=tk.TOP, fill=tk.X)
 
             a[i][k] = 0
-            print("a[i][k]=a[%d][%d]=0, postavljanje nule" % (i, k))
-            ispisi(n, m, a)
-        print("Sledeca petlja, k=%d\n" % (k + 1))
-        ispisi(n, m, a)
+            t.insert(tk.END, "a[i][k]=a[{}][{}]=0, postavljanje nule\n".format(i, k) + f"{ispisi(n, m, a)}")
+            t.pack(side=tk.TOP, fill=tk.X)
+
+        t.insert(tk.END, "Sledeca petlja, k={}\n".format(k + 1))
+        t.pack(side=tk.TOP, fill=tk.X)
+    t.insert(tk.END, f"Konacna matrica je: \n{ispisi(n, m, a)}")
+    t.pack(side=tk.TOP, fill=tk.X)
 
 
 class LinearEquationSolver:
@@ -110,7 +129,7 @@ class LinearEquationSolver:
                 matrix.append([entry if op == '+' else -entry for entry, op in zip(variables_row, operators_row)])
             result = [matrix[i] + ones_matrix[i] for i in range(3)]
             r = tk.Toplevel(self.master)
-            r.geometry('300x300')
+            r.geometry('900x400')
             show_solution(r, result)
         except ValueError:
             messagebox.showinfo("Info", "Vrednosti nisu dobre.")
